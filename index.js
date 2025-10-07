7c9bec96338fd14a967762c127251f4063ec0a66
logan <92327020+DevLoggith@users.noreply.github.com>
2025-10-07 13:02:01 -0400

    connect DB & update optional route param syntax

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import { User, Exercise } from './models.js';
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.use(cors());
 app.use(express.static("public"));
@@ -31,7 +32,7 @@ app.post("/api/users", async (req, res) => {
     } catch (err) {
         return res.json({error: err.message});
     }
-    
+
     // response has to be {username: <username>, _id: <MongoDB _id string>}
     // (use ObjectId().toString() to return MongoDB generated ID as string)
 });
@@ -47,7 +48,7 @@ app.get("/api/users", (req, res) => {
     // (simply execute .find({}) for all users documents)
 });
 
-app.get("/api/users/:_id/logs/:from?/:to?/:limit?", (req, res) => {
+app.get("/api/users/:_id/logs{/:from}{/:to}{/:limit}", (req, res) => {
     // returns a full exercise log of any user
     // returns a user object with "count" property representing the number of exercises for that user
     // returns the user object with a log field array thats an array of all the exercises added with
